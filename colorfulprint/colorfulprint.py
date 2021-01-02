50202cf0cd0ed399a9b99c7deb6f835af86765d1
Ali Emre Şafak <aliemresafak@gmail.com>
2021-01-03 01:06:45 +0300

    Added custom type

--- colorfulprint/colorfulprint.py
+++ colorfulprint/colorfulprint.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 
 class ColorfulPrint(object):
-    _DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+    _DATE_FORMAT = "%Y-%m-%d %H:%M:%S-%f"
 
     # Background is suffix B_ Foreground suffix is F_ 
     FONT_BOLD = "\x1b[1m"
     F_WHITE = "\x1b[97m"
-    
+    F_BLACK = "\x1b[30m"
     B_ERROR = "\x1b[41m"
     B_SUCCESS = "\x1b[42m"
     B_WARNING = "\x1b[43m"
     B_INFO = "\x1b[44m"
     B_DEFAULT = "\x1b[49m"
+    B_CUSTOM = "\x1b[45m"
     
+    
+    def __init__(self):
+        self.dt = datetime.now()
 
     @staticmethod
     def _generate_result(type: str, message: str):
@@ -26,25 +30,59 @@ class ColorfulPrint(object):
                 message=message)
         return result
 
+    @classmethod
+    def success(cls, message: str):
+        type = "SUCCESS"
+        result = cls._generate_result(type=type, message=message)
+        print(result)
+    
+    @classmethod
+    def error(cls, message: str):
+        type = "ERROR"
+        result = cls._generate_result(type=type, message=message)
+        print(result)
 
     @classmethod
-    def print(cls, *, type:str, message: str):
+    def warn(cls, message: str):
+        type = "WARNING"
+        result = cls._generate_result(type=type, message=message)
+        print(result)
+
+    @classmethod
+    def info(cls, message: str):
+        type = "INFO"
+        result = cls._generate_result(type=type, message=message)
+        print(result)
+
+    
+    @classmethod
+    def custom(cls, type: str, message: str):
+        result = cls._generate_result(type=type, message=message)
+        print(result)
+
+    @classmethod
+    def _generate_result(cls, *, type:str, message: str):
         dt = datetime.now().strftime(cls._DATE_FORMAT)
+        
         space_after_datetime = ' ' * 4
         style_default = f"{cls.F_WHITE}{cls.B_DEFAULT}{cls.FONT_BOLD}"
         if type == "SUCCESS":
-            space = ' ' * 2
             style = f"{cls.B_SUCCESS}{cls.F_WHITE}{cls.FONT_BOLD}"
         elif type == "ERROR":
-            space = ' ' * 4
             style = f"{cls.B_ERROR}{cls.F_WHITE}{cls.FONT_BOLD}"
         elif type == "WARNING":
-            space = ' ' * 2
-            style = f"{cls.B_WARNING}{cls.F_WHITE}{cls.FONT_BOLD}"
+            style = f"{cls.B_WARNING}{cls.F_BLACK}{cls.FONT_BOLD}"
         elif type == "INFO":
-            space = ' ' * 5
             style = f"{cls.B_INFO}{cls.F_WHITE}{cls.FONT_BOLD}"
-
+        elif type.startswith("4"):
+            style = f"{cls.B_ERROR}{cls.F_WHITE}{cls.FONT_BOLD}"
+        elif type.startswith("2"):
+            style = f"{cls.B_SUCCESS}{cls.F_WHITE}{cls.FONT_BOLD}"
+        else:
+            type = f"${type[:12]}..."
+            style = f"{cls.B_CUSTOM}{cls.F_WHITE}{cls.FONT_BOLD}"
+        
+        space = ' ' * (20 - len(type))
         result = "{style}[{type}]{style_default}{space}{dt}{space_after_dt}{message}".format(
                 style=style,
                 type=type,
@@ -53,8 +91,7 @@ class ColorfulPrint(object):
                 dt=dt,
                 space_after_dt=space_after_datetime,
                 message=message)
-
-        print(result)
+        return result
 
 
 
--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 from colorfulprint import ColorfulPrint as cfprint
 
 
-cfprint.print(type="SUCCESS", message="Deneme")
-cfprint.print(type="INFO", message="info message")
-cfprint.print(type="SUCCESS", message="success message")
-cfprint.print(type="WARNING", message="warning message")
-cfprint.print(type="ERROR", message="error message")
+cfprint.success(message="Success")
+cfprint.info(message="info message")
+cfprint.warn(message="success message")
+cfprint.error(message="warning message")
+cfprint.error(message="error message")
+cfprint.custom(type="TEIAS_API_SORGU", message="Denemeeee")
